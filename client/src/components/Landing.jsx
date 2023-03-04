defc464e6c399fed330e39db49f6402437cde549
Nancy Yu <nancyyu09@gmail.com>
2023-03-04 11:38:15 -0800

    Created Landing, VerticalNav, and VerticalNavItem components

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import VerticalNav from './VerticalNav.jsx';
+
+const Landing = props => {
+
+  return (
+    <div id='landing'>
+      <VerticalNav />
+
+    </div>
+  );
+};
+
+export default Landing;
