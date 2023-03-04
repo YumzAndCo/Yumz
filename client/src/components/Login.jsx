8f44a59fdb5c51a378f1999090772ca770f1b22a
Katrina <119702221+katmcd5@users.noreply.github.com>
2023-03-04 15:21:30 -0800

    Revert "Katrina/Login/SignupComponent"

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,51 +1,10 @@
-import React, { useState} from 'react';
-import styles from '../stylesheets/login.css'; 
-
-async function loginUser(credentials) {
-  return fetch('/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(credentials)
-  })
-    .then(data => data.json());
-}
-
+import React from 'react';
 
 // This entire portion is used for react-router setup
-export const Login = ({setToken}) => {
-
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
-  
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const token = await loginUser({
-      username,
-      password
-    });
-    setToken(token);
-  };
-
+export const Login = () => {
   return(
-    <div className="login">
-      <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Username</p>
-          <input type="text" onChange={e => setUserName(e.target.value)}/>
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)} />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+    <>
+      <h1> Hello world! Testing Testing </h1>
+    </>
   );
 };
-
