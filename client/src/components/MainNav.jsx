f426f0e9829faf858e3fcb52a4d436383d1e2e04
Nancy Yu <nancyyu09@gmail.com>
2023-03-04 14:45:18 -0800

    Created MainNav and MainNavItem components

---
+++ client/src/components/MainNav.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import MainNavItem from './MainNavItem.jsx';
+import { faBurger, faMugHot, faHeart } from '@fortawesome/free-solid-svg-icons';
+
+const MainNav = props => {
+  const onMainNavBtnClick = (event, btnName) => {
+    console.log(btnName, 'clicked!');
+  };
+
+  // Note: if we add the feature to allow users to create custom collections,
+  // then MainNavItem components can be generated on the results of a GET for a user's
+  // collections
+  return (
+    <div id="main-nav">
+      <MainNavItem
+        iconName={faBurger}
+        btnId="main-nav-reviews"
+        btnText="Reviews"
+        onClickHandler={onMainNavBtnClick} />
+      <MainNavItem
+        iconName={faMugHot}
+        btnId="main-nav-wishlist"
+        btnText="Wishlist"
+        onClickHandler={onMainNavBtnClick} />
+      <MainNavItem
+        iconName={faHeart}
+        btnId="main-nav-favorites"
+        btnText="Favorites"
+        onClickHandler={onMainNavBtnClick} />
+    </div>
+  );
+};
+
+export default MainNav;
