da1d2d24d9e4775156fdb7b9bf712f2ce4ecbd32
Nancy Yu <nancyyu09@gmail.com>
2023-03-04 14:53:39 -0800

    Added Header and MainNav components to Landing component

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import VerticalNav from './VerticalNav.jsx';
+import MainNav from './MainNav.jsx';
+import Header from './Header.jsx';
 
 const Landing = props => {
 
   return (
     <div id='landing'>
       <VerticalNav />
-
+      <Header />
+      <MainNav />
     </div>
   );
 };
