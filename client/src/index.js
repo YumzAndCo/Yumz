bb69fd8bff0006e00fe0e01a8813ea5bbc4d74b8
Nancy Yu <nancyyu09@gmail.com>
2023-03-04 15:13:54 -0800

    changed import of BrowserRouter to be from react-router-dom, commented out ReactDOM.render

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App.jsx';
 import './stylesheets/styles.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
 
-/* This portion is used for react-router
+/* This portion is used for react-router */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
-*/
+);
