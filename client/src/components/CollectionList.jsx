2b71a52c3210f8bd644fd161d64979c0f758fd4e
Katrina McDonagh <katrina@Katrinas-MacBook-Pro.local>
2023-03-06 09:44:55 -0800

    added collections list components

---
+++ client/src/components/CollectionList.jsx
@@ -0,0 +1,14 @@
+import React, { Component, useState } from 'react';
+import  '../stylesheets/listview.css';
+import ListItem from './ListItem.jsx';
+
+export const CollectionList = (props) => {
+  
+  const [restaurants, setRestaurants] = useState([{
+    props
+  }]);
+
+  return (
+    <ListItem restaurants={restaurants}/>
+  );
+};
---
+++ client/src/components/ListItem.jsx
@@ -0,0 +1,19 @@
+import React, { Component, useState } from 'react';
+
+const ListItem = (restaurants, listName) => {
+  // const restaurants = props.restaurants;
+  // const listTitle = props.title;
+
+  return (
+    <div className = "listview">
+      <div className = "collectionTitle">{listName}</div>
+      {restaurants.map((place) => 
+        (<div className="preview" key={place.id}>
+          <span>{place.name}</span> <span>{place.rating} ☆</span><span>{place.cuisine}</span><span>{place.hours}</span>
+        </div>)
+      )}
+    </div>
+  );
+};
+
+export default ListItem;
