44e40c13558ac25c1beb147dd8939619850d23bd
Nancy Yu <nancyyu09@gmail.com>
2023-03-06 10:39:34 -0800

    Fixed merge issue

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,9 +7,8 @@ import { Reviews } from './Reviews.jsx';
 import { Favorites } from './Favorites.jsx';
 import { Wishlist } from './Wishlist.jsx';
 import Landing from './Landing.jsx';
-import { CollectionList } from './CollectionList.jsx';;
+import { CollectionList } from './CollectionList.jsx';
 import DetailsModal from './DetailsModal.jsx';
-import NewRestaurant from './NewRestaurant.jsx';
 
 function App() {
 
